caaabfc4311b987d791ef81087e2f8af3fa99227
Erik Quenon Steggall <esteggal@ucsc.edu>
2014-10-24 13:34:33 -0700

    Got it reading the zip header

--- hw2/aes/brute_force.c
+++ hw2/aes/brute_force.c
@@ -7,7 +7,8 @@ int main(int argc, char* argv[]){
 
    int ret;
    char cmd[] = "./aes -d -1 ";
-   char options[] = "abcd < new.zip.crypt";
+   char key[4] = "abcd";
+   char options[] = " < test.zip.crypt";
 //   char outfile[] = ;
    char* full_cmd;
    size_t cmd_len = strlen(cmd);
@@ -24,6 +25,7 @@ int main(int argc, char* argv[]){
    printf("size of char = %lu\n", sizeof(char));
 
    full_cmd = (char *) strcat(full_cmd, cmd);
+   full_cmd = (char *) strcat(full_cmd, key);
    full_cmd = (char *) strcat(full_cmd, options);
 
    // For loop that runs through all possible permutations of
@@ -37,7 +39,7 @@ int main(int argc, char* argv[]){
    }
    //next up: Write a for loop that runs through each of the possible keys
    // create a new file with a key value of something like fedc or something...
-   /* Read the output a line at a time - output it. */
+   /* Read the output a line at a time - output it. 8 Hex chars I think. */
    while (fgets(path, sizeof(path)-1, fp) != NULL) {
       
       if(i == 0){
@@ -45,10 +47,7 @@ int main(int argc, char* argv[]){
           second_byte = path[1];
           third_byte = path[2];
           fourth_byte = path[3];
-          printf("%x\n", first_byte);
-          printf("%x\n", second_byte);
-          printf("%x\n", third_byte);
-          printf("%x\n", fourth_byte);
+          if( (first_byte == 0x50) && (second_byte == 0x4b) && (third_byte == 0x03) && (fourth_byte == 0x04))printf("truf\n");
       }
       if(first_byte == 0x04)printf("found first byte\n");
 //      printf("%s", path);
