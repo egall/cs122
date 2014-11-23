65f9c3013b3796c2601556ad82196e882828fc83
Erik Quenon Steggall <esteggal@ucsc.edu>
2014-11-22 21:16:25 -0800

    Added problem4

---
+++ hw4/prob4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void getinp(char *inp, int siz)
+{
+    puts("Input value: ");
+    fgets(inp, siz, stdin);
+    printf("buffer3 getinp read %s\n", inp);
+}
+void display(char *val)
+{
+    char tmp[16];
+    sprintf(tmp, "read val: %s\n", val);
+    puts(tmp);
+}
+int main(int argc, char *argv[])
+{
+    char buf[16];
+    getinp (buf, sizeof (buf));
+    display(buf);
+    printf("buffer3 done\n");
+}
